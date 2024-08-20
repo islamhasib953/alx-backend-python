0852bc76aee0abd467d77e6414c2d1bbed6dca7c
islamhasib953 <islamhasib953@example.com>
2024-08-20 19:55:57 +0300

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" conert float number to string """
+
+
+def to_str(n: float) -> str:
+    """function to_str takes a float n and returns the string of the float."""
+
+    return str(n)
