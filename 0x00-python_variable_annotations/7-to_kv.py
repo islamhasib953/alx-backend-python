50a4c6c974097abca1a65ede4e043f9b28259c50
islamhasib953 <islamhasib953@example.com>
2024-08-20 20:28:47 +0300

    7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,10 +2,10 @@
 
 """ function return a tuple of string and square of number as float """
 
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """function take string and number float and
     return a tuple of string and square of number as float"""
-    return (k, float(v * v))
+    return (k, v * v)
