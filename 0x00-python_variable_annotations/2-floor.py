062ca6ba6bf4243dbde1d61b7bd0d85a10a001cf
islamhasib953 <islamhasib953@example.com>
2024-08-20 19:50:40 +0300

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import math
+
+""" make float number is floor """
+
+
+def floor(n: float) -> float:
+    """function floor takes a float n and returns the floor of the float"""
+    return math.floor(n)
