1053d702fd69b9183fd54af6e7089a8f17219767
islamhasib953 <islamhasib953@example.com>
2024-08-20 19:45:33 +0300

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+""" concat two strings and return the result """
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that takes two strings and returns their concat"""
+    return str1 + str2
