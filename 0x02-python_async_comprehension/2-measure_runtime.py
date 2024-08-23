c924a4350926b9035735f83033b494704747425e
islamhasib953 <islamhasib953@example.com>
2024-08-23 19:29:46 +0300

    2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,13 +2,14 @@
 
 
 """Task 2-measure_runtime"""
-async_comprehension = __import__("1-async_comprehension").async_comprehension
 import asyncio
 import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
     """measure runtime coroutine"""
     start_time = time.time()
-    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     return time.time() - start_time
